1252a5bc3d0f77a64be3f7278c940ce6ad114797
Usuario <Usuario@CPD03-M06>
2022-09-27 12:25:57 -0300

    Adicionar arquivos de projeto.

---
+++ AppClimaAPI/AppClimaAPI/Services/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppClimaAPI.Model;
+
+namespace AppClimaAPI.Services
+{
+    class DataService
+    {
+
+        public static async Task<Clima> GetPrevisaoDoTempo(string cidade)
+        {
+            string appId = "d5bd97549255db7c953bbbf53f4e5b52";
+
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&units=metric" + "&appid=" + appId;
+            dynamic resultado = await getDataFromService(queryString).ConfigureAwait(false);
+
+            // continuar aqui (22)
+        }
+    }
+}
